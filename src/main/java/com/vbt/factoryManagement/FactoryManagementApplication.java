5d9b8827302b2321784090dac64a0b116503ce89
berkaysvm <ert.sv.bs@gmail.com>
2022-08-15 10:21:06 +0300

    Initial commit

---
+++ src/main/java/com/vbt/factoryManagement/FactoryManagementApplication.java
@@ -0,0 +1,13 @@
+package com.vbt.factoryManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FactoryManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FactoryManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vbt/factoryManagement/FactoryManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vbt.factoryManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FactoryManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
